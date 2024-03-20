3fae9c63961341e2f68f17c293889ddf46d06477
TrongNhan73 <trongnhan0703it@gmail.com>
2024-03-20 19:25:43 +0700

    add bt3

---
+++ bai3/src/App.js
@@ -0,0 +1,73 @@
+
+import './App.css';
+import imgs from './img';
+import Element from './components/Elenment';
+
+const list=[
+  {
+    key: 'nhatrang',
+    title:'Biển Nha Trang',
+    link:require('./img/nhatrang.jpg')
+  },
+  {
+    key: 'dalat',
+    title:'Thành Phố Đà Lạt',
+    link:require('./img/dalat.jpg')
+  },
+  {
+    key: 'vungtau',
+    title:'Biển Vũng Tàu',
+    link:require('./img/vungtau.jpg')
+  },
+  {
+    key: 'halong',
+    title:'Vịnh Hạ Long',
+    link:require('./img/halong.jpg')
+  },
+  {
+    key: 'phanthiet',
+    title:'Biển Phan Thiết',
+    link:require('./img/phanthiet.jpg')
+  },
+  {
+    key: 'hatien',
+    title:'Biển Hà Tiên',
+    link:require('./img/hatien.jpg')
+  },
+  {
+    key: 'phuquoc',
+    title:'Đảo Phú Quốc',
+    link:require('./img/phuquoc.jpg')
+  },
+]
+function App() {
+ 
+  return (
+    <div className="App">
+      <h1>DANH LAM THẮNG CẢNH</h1>
+      <div class='container'>
+        <div class='sights-box'>
+          {list.map(({title,key})=>{
+            let kkey='#'+key
+            return(
+              <a href={kkey}>{title}</a>
+            )
+          })}
+        </div>
+        <div class='panel'>
+          {list.map(({key,title,link})=>{
+            return(
+              <div class='img-box'>
+                <Element title={title} img={link} kkey={key}/>
+              </div>
+            )
+          })}
+        </div>
+        
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
