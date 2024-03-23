94a2bef58adf07a6296ce6b0ce9dae90da014379
thanhan2k <to.thanhan2k@gmail.com>
2024-03-23 20:16:26 +0700

    lưu tạm bai3

---
+++ bai3/src/App.js
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center ">
+      <div className="border-2 border-[#c4d381] w-[600px] mt-[60px]">
+        <h1 className="bg-[#535939] text-white font-bold text-2xl text-center py-2">DANH LAM THẮNG CẢNH</h1>
+        <div className="grid grid-cols-2 gap-4 bg-[#c4d381] h-60">
+            <div className="bg-white text-sm font-bold m-1 px-1">
+              <h3 className="text-base font-extrabold">Danh sách địa danh</h3>
+              <a href="#">Biển Nha Trang</a><br/>
+              
+            </div>
+            <div>02</div>
+        </div>
+      </div>
+    </div>
+  );
+}
